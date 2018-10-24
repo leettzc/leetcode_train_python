8f7ff9719b4bad8291b20543bcc7ebad21715771
leettzc <33587351+leettzc@users.noreply.github.com>
2018-10-24 17:22:18 +0800

    Add files via upload

---
+++ Two_Sum.py
@@ -0,0 +1,15 @@
+def twoSum( nums, target):
+	"""
+	:type nums: List[int]
+	:type target: int
+	:rtype: List[int]
+	"""
+	arr = []
+	for i in range(len(nums)):
+		for j in range(len(nums)):
+			if i != j:
+				sum = nums[i] + nums[j]
+				if sum == target:
+					arr.append(i)
+					arr.append(j)
+					return arr
