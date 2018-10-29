0aa3e35a8076b57c45f4574b4eb97c5ed615f5c0
leettzc <33587351+leettzc@users.noreply.github.com>
2018-10-29 18:07:43 +0800

    Update Algorithms_Reverse_Integer.py

--- Algorithms_Reverse_Integer.py
+++ Algorithms_Reverse_Integer.py
@@ -4,15 +4,22 @@ def reverse(x):
     :type x: int
     :rtype: int
     """
+    if x<0-(2**31):
+        return(0)
+    if x>(2**31)-1:
+        return(0)
     if x <0:
         absx = 0 - x
     arry = []
     dex= 1
+    j = 0
     while (dex):
         n = absx//10
         m = absx-10*n
         absx = n
         if m!=0:
+            j=1
+        if j!=0:
             arry.append(m)
         if n==0:
             dex = 0
@@ -25,5 +32,9 @@ def reverse(x):
         i+=1
     if x<0:
         outx = 0 - outx
+    if outx<0-(2**31):
+        return(0)
+    if outx>(2**31)-1:
+        return(0)
     return outx
 print(reverse(-324))
